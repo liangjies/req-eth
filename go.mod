4a84fa4b93ff9ba31f1cc0513f668a2f6b73dd61
liangjies <1139629972@qq.com>
2024-09-22 11:07:45 +0800

    en

--- go.mod
+++ go.mod
@@ -12,10 +12,11 @@ require (
 )
 
 require (
-	github.com/bytedance/sonic v1.11.9 // indirect
-	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/bytedance/sonic v1.12.2 // indirect
+	github.com/bytedance/sonic/loader v0.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
--- jsonrpc/json/json.go
+++ jsonrpc/json/json.go
@@ -1,15 +1,20 @@
-//go:build !jsoniter && !go_json && !(sonic && avx && (linux || windows || darwin) && amd64)
+//go:build !jsoniter && !(sonic && avx && (linux || windows || darwin) && amd64)
 
 package json
 
-import "encoding/json"
+import json "github.com/goccy/go-json"
 
 var (
-	Marshal       = json.Marshal
-	Unmarshal     = json.Unmarshal
+	// Marshal is exported by gin/json package.
+	Marshal = json.Marshal
+	// Unmarshal is exported by gin/json package.
+	Unmarshal = json.Unmarshal
+	// MarshalIndent is exported by gin/json package.
 	MarshalIndent = json.MarshalIndent
-	NewDecoder    = json.NewDecoder
-	NewEncoder    = json.NewEncoder
+	// NewDecoder is exported by gin/json package.
+	NewDecoder = json.NewDecoder
+	// NewEncoder is exported by gin/json package.
+	NewEncoder = json.NewEncoder
 )
 
 type RawMessage = json.RawMessage
